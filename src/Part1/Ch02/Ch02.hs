b071ea9b0e61326a5f9dc19c29ae0b7c27849e7b
Gustavo Franke <franke.gustavo@gmail.com>
2020-10-09 09:00:28 +0100

    add ch02

---
+++ src/Part1/Ch02/Ch02.hs
@@ -0,0 +1,14 @@
+module Part1.Ch02.Ch02 where
+
+import Part1.Ch01.Ch01
+
+-- 2 First steps
+-- 2.5 Haskell Scripts
+quadruple :: Num a => a -> a
+quadruple x = double (double x)
+
+factorial :: (Num a, Enum a) => a -> a
+factorial n = product [1..n]
+
+average :: Foldable t => t Int -> Int
+average ns = sum ns `div` length ns
