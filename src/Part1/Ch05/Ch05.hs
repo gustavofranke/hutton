1a3cc299949877c27b87a30a8736878d11562d3f
Gustavo Franke <franke.gustavo@gmail.com>
2020-10-09 09:02:25 +0100

    add ch05

---
+++ src/Part1/Ch05/Ch05.hs
@@ -0,0 +1,41 @@
+module Part1.Ch05.Ch05 where
+
+------------------------
+-- 5 List comprehensions
+------------------------
+-- 5.1 Basic concepts
+concat' :: [[a]] -> [a]
+concat' xss = [x | xs <- xss, x <- xs]
+
+firsts :: [(a, b)] -> [a]
+firsts ps = [x | (x,_) <- ps]
+
+length' :: [a] -> Int
+length' xs = sum [1 | _ <- xs]
+
+-- 5.2 Guards
+factors :: Int -> [Int]
+factors n = [x | x <- [1..n], n `mod` x == 0]
+
+prime :: Int -> Bool
+prime n = factors n == [1,n]
+
+primes :: Int -> [Int]
+primes n = [x | x <- [2..n], prime x]
+
+-- 5.3 The zip function
+pairs :: [a] -> [(a, a)]
+pairs xs = zip xs (tail xs)
+
+sorted :: Ord a => [a] -> Bool
+sorted xs = and [x <= y | (x, y) <- pairs xs]
+
+positions :: Eq a => a -> [a] -> [Int]
+positions x xs = [i | (x', i) <- zip xs [0..], x == x']
+
+-- 5.4 String comprehensions
+lowers :: String -> Int
+lowers xs = length [x | x <- xs, x >= 'a' && x <= 'z']
+
+count :: Char -> String -> Int
+count x xs = length [x' | x' <- xs, x == x']
