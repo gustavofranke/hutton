f19e477384eb304f3530dfcba774ae4320adf45d
Gustavo Franke <franke.gustavo@gmail.com>
2020-11-04 17:51:01 +0000

    format code + tidy haddock + NoImplicitPrelude, for ch05

--- src/Part1/Ch05/Ch05.hs
+++ src/Part1/Ch05/Ch05.hs
@@ -1,41 +1,76 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 -- | 5 List comprehensions
 module Part1.Ch05.Ch05 where
 
-import Prelude hiding (length)
+import Data.Foldable hiding (length)
+import GHC.Base (Bool, Char, Eq, Int, Ord, String, (&&), (<=), (==), (>=))
+import GHC.List hiding (and, length, sum)
+import GHC.Real (mod)
 
 -- | 5.1 Basic concepts
 concat :: [[a]] -> [a]
 concat xss = [x | xs <- xss, x <- xs]
 
 firsts :: [(a, b)] -> [a]
-firsts ps = [x | (x,_) <- ps]
+firsts ps = [x | (x, _) <- ps]
 
 length :: [a] -> Int
 length xs = sum [1 | _ <- xs]
 
 -- | 5.2 Guards
+-- >>> factors 15
+-- [1,3,5,15]
+--
+-- >>> factors 7
+-- [1,7]
 factors :: Int -> [Int]
-factors n = [x | x <- [1..n], n `mod` x == 0]
+factors n = [x | x <- [1 .. n], n `mod` x == 0]
 
+-- >>> prime 15
+-- False
+--
+-- >>> prime 7
+-- True
 prime :: Int -> Bool
-prime n = factors n == [1,n]
+prime n = factors n == [1, n]
 
+-- >>> primes 40
+-- [2,3,5,7,11,13,17,19,23,29,31,37]
 primes :: Int -> [Int]
-primes n = [x | x <- [2..n], prime x]
+primes n = [x | x <- [2 .. n], prime x]
+
+-- >>> find ’b’ [(’a’,1),(’b’,2),(’c’,3),(’b’,4)]
+-- [2,4]
+find :: Eq a => a -> [(a, b)] -> [b]
+find k t = [v | (k', v) <- t, k == k']
 
 -- | 5.3 The zip function
+-- >>> pairs [1,2,3,4]
+-- [(1,2),(2,3),(3,4)]
 pairs :: [a] -> [(a, a)]
 pairs xs = zip xs (tail xs)
 
+-- >>> sorted [1,2,3,4]
+-- True
+--
+-- >>> sorted [1,3,2,4]
+-- False
 sorted :: Ord a => [a] -> Bool
 sorted xs = and [x <= y | (x, y) <- pairs xs]
 
+-- >>> positions False [True, False, True, False]
+-- [1,3]
 positions :: Eq a => a -> [a] -> [Int]
-positions x xs = [i | (x', i) <- zip xs [0..], x == x']
+positions x xs = [i | (x', i) <- zip xs [0 ..], x == x']
 
 -- | 5.4 String comprehensions
+-- >>> lowers "Haskell"
+-- 6
 lowers :: String -> Int
 lowers xs = length [x | x <- xs, x >= 'a' && x <= 'z']
 
+-- >>> count 's' "Mississippi"
+-- 4
 count :: Char -> String -> Int
 count x xs = length [x' | x' <- xs, x == x']
--- src/Part1/Ch05/Cipher.hs
+++ src/Part1/Ch05/Cipher.hs
@@ -1,20 +1,46 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 -- | 5.5 The Caesar cipher
 module Part1.Ch05.Cipher where
 
-import Data.Char
+import Data.Char (chr, isLower)
+import Data.List (drop, head, minimum, sum, take, zip)
+import GHC.Base (Char, Float, Int, String, ord, otherwise, (++))
+import GHC.Num
+import GHC.Real (fromIntegral, mod, (/), (^))
 import Part1.Ch05.Ch05
 
+-- >>> let2int 'a'
+-- 0
 let2int :: Char -> Int
 let2int c = ord c - ord 'a'
 
+-- >>> int2let 0
+-- 'a'
 int2let :: Int -> Char
 int2let n = chr (ord 'a' + n)
 
+-- >>> shift 3 'a'
+-- 'd'
+
+-- >>> shift 3 'z'
+-- 'c'
+
+-- >>> shift (-3) 'c'
+-- 'z'
+
+-- >>> shift 3 ' '
+-- ''
 shift :: Int -> Char -> Char
 shift n c
-    | isLower c = int2let ((let2int c + n) `mod` 26)
-    | otherwise = c
+  | isLower c = int2let ((let2int c + n) `mod` 26)
+  | otherwise = c
 
+-- >>> encode 3 "haskell is fun"
+-- "kdvnhoo lv ixq"
+
+-- >>> encode (-3) "kdvnhoo lv ixq"
+-- "haskell is fun"
 encode :: Int -> String -> String
 encode n xs = [shift n x | x <- xs]
 
@@ -24,22 +50,32 @@ table = [8.1, 1.5, 2.8, 4.2, 12.7, 2.2, 2.0, 6.1, 7.0,
          0.2, 0.8, 4.0, 2.4, 6.7, 7.5, 1.9, 0.1, 6.0,
          6.3, 9.0, 2.8, 1.0, 2.4, 0.2, 2.0, 0.1]
 
+-- >>> percent 5 15
+-- 33.333336
 percent :: Int -> Int -> Float
 percent n m = (fromIntegral n / fromIntegral m) * 100
 
 freqs :: String -> [Float]
-freqs xs = [percent (count x xs) n | x <- ['a'..'z']]
-    where n = lowers xs
+freqs xs = [percent (count x xs) n | x <- ['a' .. 'z']]
+  where
+    n = lowers xs
 
 chisqr :: [Float] -> [Float] -> Float
-chisqr os es = sum [((o - e)^2) / e | (o, e) <- zip os es]
+chisqr os es = sum [((o - e) ^ 2) / e | (o, e) <- zip os es]
 
+-- >>> rotate 3 [1,2,3,4,5]
+-- [4,5,1,2,3]
 rotate :: Int -> [a] -> [a]
 rotate n xs = drop n xs ++ take n xs
 
+-- >>> crack "kdvnhoo lv ixq"
+-- "haskell is fun"
+
+-- >>> crack "vscd mywzboroxcsyxc kbo ecopev"
+-- "list comprehensions are useful"
 crack :: String -> String
-crack xs = encode (-factor) xs
-    where
-        factor = head (positions (minimum chitab) chitab)
-        chitab = [chisqr (rotate n table') table | n <- [0..25]]
-        table' = freqs xs
+crack xs = encode (- factor) xs
+  where
+    factor = head (positions (minimum chitab) chitab)
+    chitab = [chisqr (rotate n table') table | n <- [0 .. 25]]
+    table' = freqs xs
