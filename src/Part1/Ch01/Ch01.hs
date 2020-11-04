6375d5c3117adcd6df86381b1819bbae2fc243f4
Gustavo Franke <franke.gustavo@gmail.com>
2020-11-04 17:49:15 +0000

    format code + tidy haddock + NoImplicitPrelude, for ch01

--- src/Part1/Ch01/Ch01.hs
+++ src/Part1/Ch01/Ch01.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 -- | 1. Introduction
 module Part1.Ch01.Ch01 where
 
-import Prelude hiding (sum)
+import GHC.Base (Monad, Ord, return, (++), (<=), (>))
+import GHC.Num
 
 -- | 1.1 Functions
 -- >>> double 3
@@ -10,24 +13,28 @@ double :: Num a => a -> a
 double x = x + x
 
 -- | 1.5
+-- Defined in ‘Data.Foldable’
 -- Summing
 -- >>> sum [1,2,3]
 -- 6
 sum :: Num p => [p] -> p
 sum [] = 0
-sum (n:ns) = n + sum ns
+sum (n : ns) = n + sum ns
 
 -- | Sorting values
+-- >>> qsort [3,5,1,4,2]
+-- [1,2,3,4,5]
 qsort :: Ord a => [a] -> [a]
 qsort [] = []
-qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
-               where
-                   smaller = [a | a <- xs, a <= x]
-                   larger  = [b | b <- xs, b > x]
+qsort (x : xs) = qsort smaller ++ [x] ++ qsort larger
+  where
+    smaller = [a | a <- xs, a <= x]
+    larger = [b | b <- xs, b > x]
 
 -- | Sequencing actions
 seqn :: Monad m => [m a] -> m [a]
 seqn [] = return []
-seqn (act:acts) = do x <- act
-                     xs <- seqn acts
-                     return (x:xs)
+seqn (act : acts) = do
+  x <- act
+  xs <- seqn acts
+  return (x : xs)
