666273c078080e06f60ec8c4d882b2dcb1c94531
Gustavo Franke <franke.gustavo@gmail.com>
2020-11-05 09:01:50 +0000

    format code + tidy haddock + NoImplicitPrelude, for ch08

--- src/Part1/Ch08/Ch08.hs
+++ src/Part1/Ch08/Ch08.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+-- | 8 Declaring types and classes
 module Part1.Ch08.Ch08 where
 
---------------------------------
--- 8 Declaring types and classes
---------------------------------
--- 8.1 Type declarations
+import GHC.Base hiding (Nat)
+import GHC.Num ((+), (-), (*))
+import GHC.Real (div, (^))
+import Data.List (head)
+import GHC.Float (pi)
+
+-- | 8.1 Type declarations
 type Stringgg = [Char]
 type Pos = (Int,Int)
 
@@ -16,7 +22,7 @@ type Assoc k v = [(k,v)]
 find0 :: Eq k => k -> Assoc k v -> v
 find0 k t = head [v | (k',v) <- t, k == k']
 
--- 8.2 Data declarations
+-- | 8.2 Data declarations
 data Move = North | South | East | West
 
 move :: Move -> Pos -> Pos
@@ -57,13 +63,13 @@ safehead :: [a] -> Maybe a
 safehead [] = Nothing
 safehead xs = Just (head xs)
 
--- 8.3 Newtype declarations
+-- | 8.3 Newtype declarations
 newtype Nat0 = N0 Int
 type Nat1 = Int
 
 data Nat2 = N1 Int
 
--- 8.4 Recursive types
+-- | 8.4 Recursive types
 data Nat = Zero | Succ Nat
 nat2int :: Nat -> Int
 nat2int Zero = 0
@@ -92,11 +98,12 @@ occurs0 :: Eq a => a -> Tree a -> Bool
 occurs0 x (Leaf y) = x == y
 occurs0 x (Node l y r) = x == y || occurs0 x l || occurs0 x r
 
+-- |
+-- >>> flatten t
+-- [1,3,4,5,6,7,9]
 flatten :: Tree a -> [a]
 flatten (Leaf x) = [x]
 flatten (Node l x r) = flatten l ++ [x] ++ flatten r
-
-test12 = flatten t -- [1,3,4,5,6,7,9]
 
 occurs :: Ord a => a -> Tree a -> Bool
 occurs x (Leaf y) = x == y
@@ -107,19 +114,19 @@ occurs x (Node l y r) | x == y = True
 -- Trees in computing come in many different forms,
 -- we can declare types for trees that have:
 
--- data only in their leaves
+-- | data only in their leaves
 data Tree1 a = Leaf1 a | Node1 (Tree1 a) (Tree1 a)
 
--- data only in their nodes
+-- | data only in their nodes
 data Tree2 a = Leaf2 | Node2 (Tree2 a) a (Tree2 a)
 
--- data of different types in their leaves and nodes
+-- | data of different types in their leaves and nodes
 data Tree3 a b = Leaf3 a | Node3 (Tree3 a b) b (Tree3 a b)
 
--- or have a list of subtrees
+-- | or have a list of subtrees
 data Tree4 a = Node4 a [Tree4 a]
 
--- 8.5 Class and instance declarations
+-- | 8.5 Class and instance declarations
 class Eq0 a where
     (===), (/==) :: a -> a -> Bool
 
