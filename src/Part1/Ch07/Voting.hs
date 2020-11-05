e2e55531dd942c7285072521d2cf1a945576e66f
Gustavo Franke <franke.gustavo@gmail.com>
2020-11-05 09:01:05 +0000

    format code + tidy haddock + NoImplicitPrelude, for voting program

--- src/Part1/Ch07/Voting.hs
+++ src/Part1/Ch07/Voting.hs
@@ -1,43 +1,52 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+-- | 7.7 Voting algorithms
 module Part1.Ch07.Voting where
 
-import Data.List
+import Data.List (filter, head, last, length, sort)
+import Data.Tuple (snd)
+import GHC.Base (Eq, Int, Ord, String, map, (.), (/=), (==))
 
--- 7.7 Voting algorithms
 votes :: [String]
-votes = ["Red","Blue","Green","Blue","Blue","Red"]
+votes = ["Red", "Blue", "Green", "Blue", "Blue", "Red"]
 
+-- |
+-- >>> count1 "Red" votes
+-- 2
 count1 :: Eq a => a -> [a] -> Int
 count1 x = length . filter (== x)
 
-test6 :: Int
-test6 = count1 "Red" votes -- 2
-
+-- |
+-- >>> rmdups votes
+-- ["Red","Blue","Green"]
 rmdups :: Eq a => [a] -> [a]
 rmdups [] = []
-rmdups (x:xs) = x : filter (/= x) (rmdups xs)
-
-test7 = rmdups votes -- ["Red","Blue","Green"]
+rmdups (x : xs) = x : filter (/= x) (rmdups xs)
 
 -- result :: Ord a => [a] -> [(Int,a)]
 -- result vs = sort [(count v vs, v) | v <- rmdups vs]
 
-result :: Ord a => [a] -> [(Int,a)]
+-- |
+-- >>> result votes
+-- [(1,"Green"),(2,"Red"),(3,"Blue")]
+result :: Ord a => [a] -> [(Int, a)]
 result vs = sort [(count1 v vs, v) | v <- rmdups vs]
 
-test8 = result votes -- [(1,"Green"),(2,"Red"),(3,"Blue")]
-
+-- |
+-- >>> winner votes
+-- "Blue"
 winner :: Ord a => [a] -> a
 winner = snd . last . result
 
-test9 = winner votes -- "Blue"
-
 -- Alternative vote
 ballots :: [[String]]
-ballots = [["Red", "Green"],
-           ["Blue"],
-           ["Green","Red","Blue"],
-           ["Blue","Green","Red"],
-           ["Green"]]
+ballots =
+  [ ["Red", "Green"],
+    ["Blue"],
+    ["Green", "Red", "Blue"],
+    ["Blue", "Green", "Red"],
+    ["Green"]
+  ]
 
 rmempty :: Eq a => [[a]] -> [[a]]
 rmempty = filter (/= [])
@@ -45,16 +54,16 @@ rmempty = filter (/= [])
 elim :: Eq a => a -> [[a]] -> [[a]]
 elim x = map (filter (/= x))
 
+-- |
+-- >>> rank ballots
+-- ["Red","Blue","Green"]
 rank :: Ord a => [[a]] -> [a]
 rank = map snd . result . map head
 
-test10 :: [String]
-test10 = rank ballots -- ["Red","Blue","Green"]
-
+-- |
+-- >>> winner' ballots
+-- "Green"
 winner' :: Ord a => [[a]] -> a
 winner' bs = case rank (rmempty bs) of
-                [c] -> c
-                (c:cs) -> winner' (elim c bs)
-
-test11 :: String
-test11 = winner' ballots
+  [c] -> c
+  (c : cs) -> winner' (elim c bs)
